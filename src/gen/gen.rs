e5837fedc4a73b429ac4b4891627c60bf2f42b30
Oliver Lau <oliver@ersatzworld.net>
2025-03-27 10:38:30 +0100

    Generator compares "human-like" solved Sudoku with Sudoku solved with backtracking.

--- src/gen/gen.rs
+++ src/gen/gen.rs
@@ -26,9 +26,18 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             loop {
                 if let Some(sudoku) = Sudoku::generate(filled_cells) {
                     let sudoku_string = sudoku.to_board_string();
+                    let mut computer_sudoku = sudoku.clone();
                     let mut sudoku = sudoku;
                     if sudoku.solve_human_like() {
-                        tx.send((sudoku.difficulty(), sudoku_string)).unwrap();
+                        computer_sudoku.solve_by_backtracking();
+                        if sudoku == computer_sudoku {
+                            tx.send((sudoku.difficulty(), sudoku_string)).unwrap();
+                        } else {
+                            panic!(
+                                "Solutions differ; human-like solver:\n{}\nbacktracking:\n{}\noriginal board: {}",
+                                sudoku, computer_sudoku, sudoku_string
+                            );
+                        }
                     } else {
                         tx.send((-1.0, sudoku_string)).unwrap();
                     }
